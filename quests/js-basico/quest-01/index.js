d250a7388766a590c6b94c7208cd4d83f46736c0
Jhony Freitas <jhony00._@hotmail.com>
2022-04-01 20:37:23 -0300

    Conclusão da quest básica de javascript

---
+++ quests/js-basico/quest-01/index.js
@@ -0,0 +1,26 @@
+function somar(numero1, numero2) {
+    let soma = numero1 + numero2;
+    return 'A soma dos dois números é: ' + soma;
+}
+console.log(somar(7, 9))
+
+
+function subtrair(numero1, numero2){
+    let subtracao = numero1 - numero2;
+    return 'A subtração dos dois números é: ' + subtracao;
+}
+console.log(subtrair(50, 20))
+
+
+function dividir(numero1, numero2){
+    let divisao = numero1 / numero2;
+    return 'A divisão dos dois número é: ' + divisao; 
+}
+console.log(dividir(30, 2))
+
+
+function multiplicar(numero1, numero2){
+    let multiplicacao = numero1 * numero2;
+    return 'A multiplicação dos dois números é: ' + multiplicacao;
+}
+console.log(multiplicar(9, 6))
