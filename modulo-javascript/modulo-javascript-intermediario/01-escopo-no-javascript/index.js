3768229bb9f304f3ff4370b1d904fc611d600f02
Jhony Freitas <jhony00._@hotmail.com>
2022-05-06 22:58:29 -0300

    Adicionando exercícios 1 e 2 de js intermediário

---
+++ modulo-javascript/modulo-javascript-intermediario/01-escopo-no-javascript/index.js
@@ -0,0 +1,43 @@
+ let numero = 15
+
+
+if(true){
+    const mensagem = 'olá'
+    console.log(mensagem);
+}
+
+console.log(mensagem);
+
+
+{
+    var mensagem = 'olá'
+    console.log(mensagem);
+}
+
+console.log(mensagem);
+
+
+function falar(){
+    var mensagem = 'Olá Mundo!'
+    console.log(mensagem);
+}
+
+falar()
+console.log(mensagem);
+
+
+function falar(){
+    var mensagem ='Olá Mundo!'
+    console.log(mensagem)
+
+    function falarOutraCoisa(){
+        var mensagem = 'Adeus Mundo!'
+    }
+    console.log(falarOutraCoisa);
+}
+
+falar()
+console.log(falarOutraCoisa);
+
+
+console.log(numero);
---
+++ modulo-javascript/modulo-javascript-intermediario/02-hoisting/index.js
@@ -0,0 +1,13 @@
+var pais //declaração 
+pais = 'Brasil' //atribuição 
+
+var pais = 'Brasil'
+
+var pais 
+console.log(pais);
+pais = 'Brasil'
+
+// -----
+
+console.log(pais);
+let pais = 'Brasil'
