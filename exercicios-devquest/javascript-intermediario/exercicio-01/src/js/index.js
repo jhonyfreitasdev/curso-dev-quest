df861788bee7936b06f508b443d62f60bef707e8
Jhony Freitas <jhony00._@hotmail.com>
2022-05-31 23:20:59 -0300

    Conclusão do exercício 01 de JS intermediário

---
+++ exercicios-devquest/javascript-intermediario/exercicio-01/src/js/index.js
@@ -0,0 +1,36 @@
+let setaAvancar = document.getElementById("avancar");  
+let setaVoltar = document.getElementById("voltar");
+let listaDeFundos = document.getElementsByClassName("fundo");
+let fundoAtual = 0;
+let fundoAnterior = 0;
+
+function esconderFundo(){
+    listaDeFundos[fundoAnterior].classList.remove('mostrar-fundo');
+}
+
+function desabilitarSeta(){
+    const primeiroFundo = 0;
+    const ultimoFundo = listaDeFundos.length -1;
+
+    fundoAtual === primeiroFundo ? setaVoltar.classList.add('desabilitar-seta') : setaVoltar.classList.remove('desabilitar-seta');
+
+    fundoAtual === ultimoFundo ? setaAvancar.classList.add('desabilitar-seta') : setaAvancar.classList.remove('desabilitar-seta');
+}
+
+setaAvancar.addEventListener('click', function(){
+    fundoAnterior = fundoAtual
+    fundoAtual ++;
+    listaDeFundos[fundoAtual].classList.add("mostrar-fundo");    
+    esconderFundo();
+
+    desabilitarSeta();
+})
+
+setaVoltar.addEventListener('click', function(){
+    fundoAnterior = fundoAtual
+    fundoAtual --;
+    listaDeFundos[fundoAtual].classList.add("mostrar-fundo");    
+    esconderFundo();
+
+    desabilitarSeta();
+})
