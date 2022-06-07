03ba3a3720da2a674cd45de5b0eb22692d445ced
Jhony Freitas <jhony00._@hotmail.com>
2022-06-06 22:10:17 -0300

    Alterações de clean code

--- exercicios-devquest/javascript-intermediario/exercicio-01/src/js/index.js
+++ exercicios-devquest/javascript-intermediario/exercicio-01/src/js/index.js
@@ -4,6 +4,30 @@ let listaDeFundos = document.getElementsByClassName("fundo");
 let fundoAtual = 0;
 let fundoAnterior = 0;
 
+
+
+setaAvancar.addEventListener('click', function(){
+    fundoAnterior = fundoAtual;
+    fundoAtual ++;
+
+    mostrarFundo();    
+    esconderFundo();
+    desabilitarSeta();
+})
+
+setaVoltar.addEventListener('click', function(){
+    fundoAnterior = fundoAtual;
+    fundoAtual --;
+
+    mostrarFundo();
+    esconderFundo();
+    desabilitarSeta();
+});
+
+function mostrarFundo() {
+    listaDeFundos[fundoAtual].classList.add("mostrar-fundo");
+}
+
 function esconderFundo(){
     listaDeFundos[fundoAnterior].classList.remove('mostrar-fundo');
 }
@@ -16,22 +40,3 @@ function desabilitarSeta(){
 
     fundoAtual === ultimoFundo ? setaAvancar.classList.add('desabilitar-seta') : setaAvancar.classList.remove('desabilitar-seta');
 }
-
-setaAvancar.addEventListener('click', function(){
-    fundoAnterior = fundoAtual
-    fundoAtual ++;
-    listaDeFundos[fundoAtual].classList.add("mostrar-fundo");    
-    
-    esconderFundo();
-
-    desabilitarSeta();
-})
-
-setaVoltar.addEventListener('click', function(){
-    fundoAnterior = fundoAtual
-    fundoAtual --;
-    listaDeFundos[fundoAtual].classList.add("mostrar-fundo");    
-    esconderFundo();
-
-    desabilitarSeta();
-})
