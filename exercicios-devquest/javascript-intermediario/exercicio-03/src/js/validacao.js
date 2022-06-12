72f34dd055190c4e92a47fb091704c41b6f98086
Jhony Freitas <jhony00._@hotmail.com>
2022-06-12 18:18:16 -0300

    Aquele clean code que todo mundo gosta

--- exercicios-devquest/javascript-intermediario/exercicio-03/src/js/validacao.js
+++ exercicios-devquest/javascript-intermediario/exercicio-03/src/js/validacao.js
@@ -1,88 +1,23 @@
-let campos = document.querySelectorAll(".campos")
+let campos = document.querySelectorAll(".campo");
 let botaoEnviar = document.getElementById("btn-enviar");
-let nome = document.getElementById("nome");
-let email = document.getElementById("email");
-let telefone = document.getElementById("telefone");
-let mensagem = document.getElementById("mensagem");
-let avisoObrigatorio = document.querySelectorAll(".obrigatorio");
 
 campos.forEach(function(campo){
     campo.addEventListener('change', function(){
+        const idVerificacaoValido = campo.id;
         if(campo.value !== ""){
-            campo.classList.add("valido");
-            campo.classList.remove("invalido");
+            document.getElementsByClassName(idVerificacaoValido)[0].classList.add("valido");
+            document.getElementsByClassName(idVerificacaoValido)[0].classList.remove("invalido");
         }else{
-            campo.classList.remove("valido");
+            document.getElementsByClassName(idVerificacaoValido)[0].classList.remove("valido");
         }
     });
 });
 
 botaoEnviar.addEventListener('click', function(){
-    campoVazio = ""
-    if(nome.value === campoVazio){
-        nome.classList.add("invalido")
-        avisoObrigatorio[0].classList.add("invalido")
-    }else{
-        nome.classList.remove("invalido")
-        avisoObrigatorio[0].classList.remove("invalido")
-        nome.classList.add("valido")
-        console.log('ta entrando');
-    }
-
-    if(email.value === campoVazio){
-        email.classList.add("invalido")
-        avisoObrigatorio[1].classList.add("invalido")
-    }else{
-        email.classList.remove("invalido")
-        avisoObrigatorio[1].classList.remove("invalido")
-        email.classList.add("valido")
-        console.log('ta entrando');
-    }
-
-    if(telefone.value === campoVazio){
-        telefone.classList.add("invalido")
-        avisoObrigatorio[2].classList.add("invalido")
-    }else{
-        telefone.classList.remove("invalido")
-        avisoObrigatorio[2].classList.remove("invalido")
-        telefone.classList.add("valido")
-        console.log('ta entrando');
-    }
-
-    if(mensagem.value === campoVazio){
-        mensagem.classList.add("invalido")
-        avisoObrigatorio[3].classList.add("invalido")
-    }else{
-        mensagem.classList.remove("invalido")
-        avisoObrigatorio[3].classList.remove("invalido")
-        mensagem.classList.add("valido")
-        console.log('ta entrando');
-    }
+    campos.forEach(function(campo){
+        const idVerificacaoInvalido = campo.id;
+        if(campo.value === ""){
+            document.getElementsByClassName(idVerificacaoInvalido)[0].classList.add("invalido");
+        };
+    });
 });
-
-
-
-// botaoEnviar.addEventListener('click', function(){
-//     campos.forEach(function(campo){
-//         if(campo.value === ""){
-//             campo.classList.add("invalido");
-//             const idDeReferenciaInvalida = campo.id;
-//                 if(idDeReferenciaInvalida === 'nome'){
-//                     avisoObrigatorio[0].classList.add("invalido");
-//                 }else if(idDeReferenciaInvalida === "email"){
-//                     avisoObrigatorio[1].classList.add("invalido");
-//                 }else if(idDeReferenciaInvalida === "telefone"){
-//                     avisoObrigatorio[2].classList.add("invalido");
-//                 }else if(idDeReferenciaInvalida === "mensagem"){
-//                     avisoObrigatorio[3].classList.add("invalido");
-//                 };
-//         }else{
-//             campo.classList.remove("invalido");
-//             avisoObrigatorio[0].classList.remove("invalido");
-//         };
-//     });
-// });
-
-
-
-
